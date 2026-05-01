99bb19fc45d5bb6803d717bda2a8de810342ad12
Apurba Debnath <apurba871@gmail.com>
2026-05-01 12:35:31 +0530

    add job seeker info

---
+++ jobportal/src/main/java/com/dev/jobportal/controller/JobSeekerProfileController.java
@@ -0,0 +1,56 @@
+package com.dev.jobportal.controller;
+
+import com.dev.jobportal.entity.JobSeekerProfile;
+import com.dev.jobportal.entity.Skills;
+import com.dev.jobportal.entity.Users;
+import com.dev.jobportal.repository.UsersRepository;
+import com.dev.jobportal.services.JobSeekerProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/job-seeker-profile")
+public class JobSeekerProfileController {
+    private final JobSeekerProfileService jobSeekerProfileService;
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public JobSeekerProfileController(JobSeekerProfileService jobSeekerProfileService, UsersRepository usersRepository) {
+        this.jobSeekerProfileService = jobSeekerProfileService;
+        this.usersRepository = usersRepository;
+    }
+
+    @GetMapping("/")
+    public String JobSeekerProfile(Model model) {
+        JobSeekerProfile jobSeekerProfile = new JobSeekerProfile();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        List<Skills> skills = new ArrayList<>();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            Users user = usersRepository.findByEmail(authentication.getName()).orElseThrow(() ->
+                    new UsernameNotFoundException("User not found"));
+            Optional<JobSeekerProfile> seekerProfile = jobSeekerProfileService.getOne(user.getUserId());
+            if (seekerProfile.isPresent()) {
+                jobSeekerProfile = seekerProfile.get();
+                if (jobSeekerProfile.getSkills().isEmpty()) {
+                    skills.add(new Skills());
+                    jobSeekerProfile.setSkills(skills);
+                }
+            }
+            model.addAttribute("skills", skills);
+            model.addAttribute("profile", jobSeekerProfile);
+        }
+
+        return "job-seeker-profile";
+    }
+}
---
+++ jobportal/src/main/java/com/dev/jobportal/services/JobSeekerProfileService.java
@@ -0,0 +1,20 @@
+package com.dev.jobportal.services;
+
+import com.dev.jobportal.entity.JobSeekerProfile;
+import com.dev.jobportal.repository.JobSeekerProfileRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class JobSeekerProfileService {
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+
+    public JobSeekerProfileService(JobSeekerProfileRepository jobSeekerProfileRepository) {
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+    }
+
+    public Optional<JobSeekerProfile> getOne(Integer id) {
+        return jobSeekerProfileRepository.findById(id);
+    }
+}
